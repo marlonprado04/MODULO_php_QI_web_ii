4ad71543b71f40ca911e4cf700fd3211f8f06664
Marlon Prado <marlonprado04@gmail.com>
2023-10-23 16:08:52 -0300

    style: change folder structure

---
+++ semana_01/apostila/exemplos/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meus testes</title>
+</head>
+
+<body>
+
+  <?php
+    echo "Olá, eu sou um script feito em PHP, que printa algo em uma página WEB."
+  ?>
+
+</body>
+
+</html>
