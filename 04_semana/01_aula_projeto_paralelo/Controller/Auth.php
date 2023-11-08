a82d6d91f5c43e21259b7ba1c6c6018fe6885e31
Marlon Prado <marlonprado04@gmail.com>
2023-11-07 21:55:32 -0300

    feat: add aula 01 semana 4

--- 04_semana/01_aula_projeto_paralelo/Controller/Auth.php
+++ 04_semana/01_aula_projeto_paralelo/Controller/Auth.php
@@ -2,12 +2,15 @@
 // Iniciando sessão
 session_start();
 
+// Criando função de login
+function login (){
+
 // Verificando se POST está vazio
 if (empty($_POST)) {
   // No caso de vazio, redireciona para outra página
   $_SESSION["msg_error"] = "Ops. Houve um erro ao acessar esta página";
   // Redirecionando para página de mensagem
-  header("location:message.php");
+  header("location:../View/message.php");
   // Fechando sessão
   exit;
 
@@ -37,9 +40,9 @@ if (empty($_POST)) {
     // Verifica se email e senha digitados são iguais aos do usuário percorrido atualmente
     if ($user["email"] == $email && password_verify($password, $user["password"])) {
       // Se o email e senha batem, cria uma sessão com nome do usuário
-      $_SESSION["name"] = $user["name"];
+      $_SESSION["user_data"] = $user["name"];
       // Redirecionando para a tela de dashboard
-      header("location:dashboard.php");
+      header("location:../View/dashboard.php");
       // Saindo após redirecionar o usuário
       exit;
     }
@@ -48,9 +51,35 @@ if (empty($_POST)) {
 
   // Caso a varredura não encontre usuário, armazenando informação de erro
   $_SESSION["msg_warning"] = "Usuário ou senha inválidos!!";
-  header("location:message.php");
+  header("location:../View/message.php");
   exit;
 
+}
+
+}
+
+// Criando função de logout
+function logout(){
+  // Destruindo variável de sessão
+  unset($_SESSION["user_data"]);
+  // Redirecionando usuário tela de login
+  header("location:../index.php");
+  // Saindo
+  exit;
+}
+
+// Criando switch para selecionar usar a função de acordo com operação passada na requisição
+switch($_GET["operation"]){
+  case "login":
+    login();
+    break;
+  case "logout":
+    logout();
+    break;
+  default:
+    $_SESSION["msg_warning"] = "Operação inválida!!!";
+    header("location:../View/message.php");
+    exit;
 }
 
 ?>
--- 04_semana/01_aula_projeto_paralelo/View/message.php
+++ 04_semana/01_aula_projeto_paralelo/View/message.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>PHP Desk Helper - Mensagem do sistema</title>
 </head>
 
@@ -19,12 +21,12 @@
   <?php
   // Verificando se mensagem de erro não está vazia
   if (!$_SESSION["msg_error"]):
-    ?>
+  ?>
 
     <!-- Se sessão não estiver vazia, renderiza mensagem de erro com valor armazenado-->
     <div class="alert alert-danger">
       <p>
-        <?= $_SESSION["msg_error"]; ?>
+        <?=$_SESSION["msg_error"]?>
       </p>
     </div>
 
@@ -33,6 +35,22 @@
 
     // Destruindo informação da sessão após imprimir ela em tela
     unset($_SESSION["msg_error"]);
+  endif;
+  ?>
+
+  <?php
+  // Verificando se mensagem de erro não está vazia
+  if (!$_SESSION["msg_warning"]):
+    ?>
+
+    <div class="alert alert-warning">
+      <p>
+        <?=$_SESSION["msg_warning"]?>
+      </p>
+    </div>
+
+    <?php
+    unset($_SESSION["msg_warning"]);
   endif;
   ?>
 
