95c93149f188a819fa24a11a175bf2a9cda0d6d2
Marlon Prado <marlonprado04@gmail.com>
2023-11-14 21:14:02 -0300

    feat: add exemplo de array

--- 02_semana/apostila/exemplos/arrays_desestruturando.php
+++ 02_semana/apostila/exemplos/arrays_desestruturando.php
@@ -1,8 +1,16 @@
 <?php
 
+// Criando array de frutas
 $frutas = ["maçã", "banana", "laranja"];
 
+// Desestruturando o array em variáveis individuais, com o método list
 list($fruta1, $fruta2, $fruta3) = $frutas;
+
+// Forma alternativa de desestruturar, sem usar o método list 
 [$fruta1, $fruta2, $fruta3] = $frutas;
 
+// Printando no console
+echo $fruta1 . PHP_EOL;
+echo $fruta2 . PHP_EOL;
+echo $fruta3 . PHP_EOL;
 ?>
